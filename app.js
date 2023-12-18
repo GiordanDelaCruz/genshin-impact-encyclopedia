49d9325f12ce456c1ac9fb825d900de99c0c7985
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2023-12-18 15:34:19 -0500

    Added route to handle charcter query and load data into EJS template.

--- app.js
+++ app.js
@@ -18,14 +18,43 @@ app.set("view engine", "ejs");      // Load EJS templates
 /***********************************************************/
 app.use(express.static("public"));
 app.use(morgan("dev"));
-// app.use(bodyParser)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /***********************************************************/
 /******             Route Handling                    ******/
 /***********************************************************/
 // Get Homepage
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    try{
+        res.render("index");
+    }catch(error) {
+        console.log(error);
+    }
+});
+
+// Handle Character Query
+app.post("/post-character", async (req, res) => {
+    const character = req.body.character;
+    console.log("Character Query: " + character);
+    try{
+        const result = await axios.get(API_URL + "/characters/" + character);
+        res.render("index", {
+            name:  result.data.name,
+            title: result.data.title, 
+            vision: result.data.vision, 
+            weapon: result.data.weapon, 
+            gender: result.data.gender, 
+            nation: result.data.nation, 
+            affiliation: result.data.affiliation, 
+            rarity: result.data.rarity, 
+            release: result.data.release, 
+            constellation: result.data.constellation, 
+            birthday: result.data.birthday, 
+            description: result.data.description
+        });
+    }catch(error) {
+        console.log(error);
+    }
 });
 
 // TESTING
@@ -33,7 +62,18 @@ app.get("/test", async (req, res) => {
     try{
         const result = await axios.get(API_URL + "/characters/ganyu");
         res.render("index", {
-            data: JSON.stringify(result.data)
+            name:  result.data.name,
+            title: result.data.title, 
+            vision: result.data.vision, 
+            weapon: result.data.weapon, 
+            gender: result.data.gender, 
+            nation: result.data.nation, 
+            affiliation: result.data.affiliation, 
+            rarity: result.data.rarity, 
+            release: result.data.release, 
+            constellation: result.data.constellation, 
+            birthday: result.data.birthday, 
+            description: result.data.description
         });
     }catch(error) {
         console.log(error);
