068d51a0c7927fd3f9f7e64c8bd0e11e8521acd6
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2023-12-31 02:36:49 -0500

    Added code to run server remotely.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 import { type } from "os";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));  
 const API_URL = "https://genshin.jmp.blue";
 
